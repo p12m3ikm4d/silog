b4eb89a2dbf603ea7720a052e537c5cbd6a84ed1
songchihyun <sch@samwooim.com>
2024-03-04 18:47:02 +0900

    mod setup py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="silog",
-    version="0.1.0",
+    version="0.1.1",
     author="p12m3ikm4d",
     author_email="issue.no9@gmail.com",
     description="A simple logging library",
